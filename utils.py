89fe158a853a51b5054d7045d6a9d9e6b3aceb58
Simon Haxby <simon.haxby@gmail.com>
2018-02-23 19:37:20 -0800

    utils updated

--- utils.py
+++ utils.py
@@ -1,9 +1,18 @@
 import yaml
+import sys
+
 
 def load_yaml(filename):
-	with open(filename, 'r') as f_obj:
-		return yaml.load(f_obj)
+    with open(filename, 'r') as f_obj:
+        return yaml.load(f_obj)
 
 
 def empty_lists(n):
     return [[] for _ in range(n)]
+
+
+# def verbose_print(verbose, out):
+#     if verbose == 1:
+#         print(out)
+#     elif verbose == 2:
+#         print(out, file=sys.stderr)
