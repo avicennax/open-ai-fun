df51fa02b743a7e2ef09cb1b1339f8a790839bbe
avicennax <simon.haxby@gmail.com>
2018-07-28 02:57:59 -0700

    RF mv render script

--- experiments/run.sh
+++
@@ -1,3 +0,0 @@
-ipython -- render.py \
-    --meta-graph-path ./experiments/models/model-run-0.ckpt-700.meta \
-    --config ./experiments/configs/cartpole_config.yml
---
+++ experiments/scripts/run.sh
@@ -0,0 +1,3 @@
+ipython -- render.py \
+    --meta-graph-path ../models/model-run-0.ckpt-700.meta \
+    --config ../configs/cartpole_config.yml
